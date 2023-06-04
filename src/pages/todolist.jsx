efa0d072581377c3242993c36d4927e53834bbbc
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-06-04 18:25:48 +0200

    uprava page todolist

--- src/pages/todolist.jsx
+++ src/pages/todolist.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '@/styles/Home.module.css';
 import Menu from '../components/Menu/Menu.jsx';
+import Image from 'next/image';
 
 const Todolist = () => {
   return (
@@ -8,6 +9,14 @@ const Todolist = () => {
       <nav className={styles.hamburgerMenu}>
         <Menu />
       </nav>
+      <Image
+        src="/Taskmate_Mrk.gif"
+        alt="taskmate2"
+        className={styles.taskmateTodo}
+        width={300}
+        height={300}
+        priority
+      />
     </div>
   );
 };
