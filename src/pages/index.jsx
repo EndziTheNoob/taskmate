8a30e76e6268279923f26168d5613c5d284de111
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-06-04 14:45:26 +0200

    uvodni nastaveni

---
+++ src/pages/index.jsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+
+// uprava fontu
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TaskMate</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={`${styles.main}`}>
+        <div className={styles.description}>
+          <Image
+            src="/logo_bublina.png"
+            alt="logo"
+            className={styles.taskmatelogo}
+            width={600}
+            height={200}
+            priority
+          />
+          <p>
+            To-Do list for those who need to push a little bit.
+            <br />
+            Absolutely free.
+            <br />
+            Are you ready?
+          </p>
+          <a className={styles.buttonStart}>Start</a>
+        </div>
+      </main>
+    </>
+  );
+}
