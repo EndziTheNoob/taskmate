6951a97d20339d797b225be27672e0f20f9782fb
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-06-10 19:17:43 +0200

    supabase funkce prihlaseni

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,5 @@
+import { supabase } from './supabase';
+
 export function SaveUserName(name) {
   localStorage.setItem('name', name);
 }
@@ -5,3 +7,20 @@ export function SaveUserName(name) {
 export function LoadUserName() {
   return localStorage.getItem('name') ?? '';
 }
+
+export const signUp = (email, password) => {
+  const supabase = getSupabase();
+  return supabase.auth.signUp({ email, password });
+};
+
+export const signIn = (email, password) => {
+  return supabase.auth.signInWithPassword({ email, password });
+};
+
+export const signOut = () => {
+  return supabase.auth.signOut();
+};
+
+export const getSession = () => {
+  return supabase.auth.getSession();
+};
