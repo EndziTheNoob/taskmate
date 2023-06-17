6d1c289616e13696953b33bd45238a1c032331c3
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-06-17 12:00:56 +0200

    uprava a presunuti styles k login ikonce

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import { getUserEmail, signOut } from '@/services/user';
 import Menu from '../Menu';
-import { Nav } from './styled';
+import { Nav, DivLog } from './styled';
 import { useEffect, useState } from 'react';
 import { ImExit } from 'react-icons/im';
 import { FaUser } from 'react-icons/fa';
@@ -34,27 +34,7 @@ const Header = () => {
           size={25}
           style={{ cursor: 'pointer' }}
         />
-        {isOpen && userEmail && (
-          <div
-            style={{
-              position: 'absolute',
-              top: '100%',
-              left: '50%',
-              transform: 'translateX(-60%)',
-              color: '#666666',
-              padding: '4px',
-              borderRadius: '1rem',
-              marginRight: '1rem',
-              marginTop: '0.7rem',
-              marginBottom: '2rem',
-              backgroundColor: '#B6ACBD',
-              fontFamily: 'Krona One',
-              fontSize: '10px',
-            }}
-          >
-            {userEmail}
-          </div>
-        )}
+        {isOpen && userEmail && <DivLog>{userEmail}</DivLog>}
       </div>
       <button onClick={handleLogout}>
         <ImExit size={25} />
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Palette } from '../../styles/Palette';
 
 export const Nav = styled.nav`
   display: flex;
@@ -18,4 +19,21 @@ export const Nav = styled.nav`
     align-self: baseline;
     font-family: var(--font-mono);
   }
+`;
+
+export const DivLog = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-60%);
+  color: #666666;
+  padding: 4px;
+  border-radius: 6px;
+  margin-right: 1rem;
+  margin-top: 0.7rem;
+  margin-bottom: 2rem;
+  background-color: ${Palette.purple_light};
+  font-family: 'Krona One';
+  font-size: 10px;
+  padding: 0.5rem;
 `;
